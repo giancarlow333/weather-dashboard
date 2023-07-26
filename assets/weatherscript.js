804a67243a5d472dce1c2c704b0b8b5640d529ec
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-25 21:57:31 -0700

    Clean weatherscript.js

--- assets/weatherscript.js
+++ assets/weatherscript.js
@@ -7,10 +7,6 @@ var longitude;
 var APIname;
 var weatherQueryURL;
 
-// https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
-
-//fetch(queryURL);
-
 /* Fetch location data
  * Uses geocoding API
  * (https://openweathermap.org/api/geocoding-api)
@@ -18,28 +14,23 @@ var weatherQueryURL;
 city = "Los Angeles"; // for testing
 var geocodeQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
 
-console.log(city);
-console.log(geocodeQueryURL);
-
 async function buttonClickHandler(event) {
     event.preventDefault();
-    console.log("HELLO!");
+    
     await fetch(geocodeQueryURL).then(async function (response) {
         await response.json().then(function (data) {
             APIname = data[0].name;
             latitude = data[0].lat;
             longitude = data[0].lon;
-            console.log("Call#1: ", longitude);
-            
         });
     });
+    
     weatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
-    console.log("Call#2: ", longitude);
-    console.log("weatherQueryURL: ", weatherQueryURL);
+    
     await fetch(weatherQueryURL).then(function (response) {
         response.json().then(function (data) {
             console.log(data);
-            console.log("Call#3: ", longitude);
+            // Store pertinent results into local storage
         });
     });
 };
